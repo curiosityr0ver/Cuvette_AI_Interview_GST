876a9f320767496d5bc777c0714aa2590631d8ad
Ishu Mehta <mehta.ishu14@gmail.com>
2024-06-25 14:57:53 +0530

    refactor: Simplify routing in main.jsx and update asset paths in index.html

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -14,10 +14,7 @@ const questions = [
 createRoot(document.getElementById("root")).render(
 	<BrowserRouter>
 		<Routes>
-			<Route
-				path="/questionnaire"
-				element={<Questionnaire questions={questions} />}
-			/>
+			<Route path="/" element={<Questionnaire questions={questions} />} />
 		</Routes>
 	</BrowserRouter>
 );
