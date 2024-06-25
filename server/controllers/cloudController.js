efb024416087d1fb349cb7fe86f7987e1f75dbdb
Ishu Mehta <mehta.ishu14@gmail.com>
2024-06-25 14:26:40 +0530

    chore: Update Google Cloud credentials in cloudController.js

--- server/controllers/cloudController.js
+++ server/controllers/cloudController.js
@@ -1,8 +1,22 @@
 const { SpeechClient } = require('@google-cloud/speech');
 const fs = require('fs');
 
+const credentials = {
+    type: process.env.GC_TYPE,
+    project_id: process.env.GC_PROJECT_ID,
+    private_key_id: process.env.GC_PRIVATE_KEY_ID,
+    private_key: process.env.GC_PRIVATE_KEY.replace(/\\n/g, '\n'),  // Handle line breaks
+    client_email: process.env.GC_CLIENT_EMAIL,
+    client_id: process.env.GC_CLIENT_ID,
+    auth_uri: process.env.GC_AUTH_URI,
+    token_uri: process.env.GC_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.GC_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.GC_CLIENT_X509_CERT_URL
+};
+
+
 // Initialize the Google Speech client
-const speechClient = new SpeechClient();
+const speechClient = new SpeechClient({ credentials });
 
 const transcribeAudio = async (req, res) => {
     console.log("Transcribing audio...");
